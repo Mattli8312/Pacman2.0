66016bf8bfdbf0c6f80fcc007ec038797e316071
Matthew Lian <mlian5@illinois.edu>
2021-07-26 19:07:48 -0500

    updated

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ int main(int argc, char *argv[])
     const float FPS = 60;
     const float frame_delay = 1000.0/FPS;
     Game game;
-    game.Init();
     std::cout<<"Game Initialized"<<std::endl;
     std::cout<<"frame delay:"<<frame_delay<<std::endl;
+    game.Init();
     while(game.IsRunning()){
         game.EventListener();
+        game.HandleCollision();
         game.Update();
         game.Render();
         SDL_Delay(frame_delay);
     }
-    std::cout<<"Clearing"<<std::endl;
+    std::cout<<"Clearing..."<<std::endl;
     game.Clear();
     std::cout<<"Terminated"<<std::endl;
     return 0;
